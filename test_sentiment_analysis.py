70af34017a3869bb4333207752f54db771d6a80a
Emmanuel Oyewole <oyewoleemmanuel300@gmail.com>
2024-01-12 05:57:09 +0100

    first commit

---
+++ test_sentiment_analysis.py
@@ -0,0 +1,13 @@
+import unittest
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
+
+class TestSentimentAnalysis(unittest.TestCase):
+    def test_sentiment_analyzer(self):
+        test_1 = sentiment_analyzer("i love python")
+        self.assertEqual(test_1['label'], 'SENT_POSITIVE')
+        test_2 = sentiment_analyzer("i hate python")
+        self.assertEqual(test_2['label'], 'SENT_NEGATIVE')
+        test_3 = sentiment_analyzer("i'm neutral with python")
+        self.assertEqual(test_3['label'], 'SENT_NUETRAL')
+
+unittest.main()
